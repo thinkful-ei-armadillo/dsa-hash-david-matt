36624f354b1a37fffc53792b1fe25d24ad96fac7
David Haugen <haugen.dj@gmail.com>
2019-04-08 16:04:59 -0500

    separate chaining start

--- HashMap.js
+++ HashMap.js
@@ -52,7 +52,7 @@ class HashMap {
     for (let i = start; i < start + this._capacity; i++) {
       const index = i % this._capacity;
       const slot = this._hashTable[index];
-      if (slot === undefined || (slot.key === key && !slot.DELETED)) { // this was originally !slot.deleted
+      if (slot === undefined || (slot.key === key && !slot.DELETED)) {
         return index;
       }
     }
@@ -194,14 +194,135 @@ function anagram(list) {
   const hm = new Map();
 
   for(let i = 0; i < list.length; i++) {
-    const sorted = sortHelper(list[i])
+    const sorted = sortHelper(list[i]);
     const group = hm.get(sorted) || [];
-    hm.set(sorted, [...group,list[i]])
+    hm.set(sorted, [...group,list[i]]);
     
   }
 
-  return Array.from(hm.values())
+  return Array.from(hm.values());
 }
 
 
-console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']))
+// console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
+
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class HashMapChaining {
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._hashTable = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._hashTable[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._hashTable[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > this.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * this.SIZE_RATIO);
+    }
+    //Find the slot where this key should be in
+    const index = this._findSlot(key);
+
+    if (!this._hashTable[index]) {
+      this.length++;
+    }
+    try {
+      this.get(key)
+      let currentNode = this._hashTable.head;
+      while(!currentNode.next){
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new _Node(value);
+    } catch(err) {
+      const node = new _Node(value);
+      this._hashTable[index] = {head: node};
+    }
+    // this._hashTable[index] = {
+    //   key,
+    //   value,
+    //   DELETED: false
+    // };
+  }
+
+  delete(key) {
+    const index = this._findSlot(key);
+    const slot = this._hashTable[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.DELETED = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMapChaining._hashString(key);
+    const index = hash % this._capacity;
+    return index;
+  }
+
+  _resize(size) {
+    const oldSlots = this._hashTable;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._hashTable = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.DELETED) { // this was originally !slot.deleted
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      //Bitwise left shift with 5 0s - this would be similar to
+      //hash*31, 31 being the decent prime number
+      //but bit shifting is a faster way to do this
+      //tradeoff is understandability
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      //converting hash to a 32 bit integer
+      hash = hash & hash;
+    }
+    //making sure has is unsigned - meaning non-negtive number. 
+    return hash >>> 0;
+  }
+}
+
+function mainHashChain() {
+  const lor = new HashMapChaining();
+  lor.MAX_LOAD_RATIO = 0.5;
+  lor.SIZE_RATIO = 3;
+
+  lor.set('Hobbit','Bilbo');
+  lor.set('Hobbit','Frodo');
+  lor.set('Human','Aragon');
+  // lor.set('Wizard','Gandolf');
+  lor.set('Elf','Legolas');
+  // lor.set('Maiar','Sauron');
+  lor.set('LadyOfLight','Galadriel');
+  lor.set('RingBearer','Gollum');
+  lor.set('HalfElven','Arwen');
+  // lor.set('Ent','Treebeard');
+
+  console.log(lor._hashTable);
+}
+
+mainHashChain();
