a85cb51fc9fe7fb18da66d0904e1e9859f2e264f
matth3wn <matth3wn@gmail.com>
2019-04-08 09:11:52 -0700

    finished first two drills

---
+++ HashMap.js
@@ -0,0 +1,135 @@
+class HashMap {
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._hashTable = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._hashTable[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._hashTable[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > this.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * this.SIZE_RATIO);
+    }
+    //Find the slot where this key should be in
+    const index = this._findSlot(key);
+
+    if (!this._hashTable[index]) {
+      this.length++;
+    }
+    this._hashTable[index] = {
+      key,
+      value,
+      DELETED: false
+    };
+  }
+
+  delete(key) {
+    const index = this._findSlot(key);
+    const slot = this._hashTable[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.DELETED = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._hashTable[index];
+      if (slot === undefined || (slot.key === key && !slot.DELETED)) { // this was originally !slot.deleted
+        return index;
+      }
+    }
+  }
+
+  _resize(size) {
+    const oldSlots = this._hashTable;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._hashTable = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.DELETED) { // this was originally !slot.deleted
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      //Bitwise left shift with 5 0s - this would be similar to
+      //hash*31, 31 being the decent prime number
+      //but bit shifting is a faster way to do this
+      //tradeoff is understandability
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      //converting hash to a 32 bit integer
+      hash = hash & hash;
+    }
+    //making sure has is unsigned - meaning non-negtive number. 
+    return hash >>> 0;
+  }
+}
+
+const WhatDoesThisDo = function(){
+  let str1 = 'Hello World.';
+  let str2 = 'Hello World.';
+  let map1 = new HashMap();
+  map1.set(str1,10);
+  map1.set(str2,20);
+  let map2 = new HashMap();
+  let str3 = str1;
+  let str4 = str2;
+  map2.set(str3,20);
+  map2.set(str4,10);
+
+  console.log(map1.get(str1));
+  console.log(map2.get(str3));
+}
+
+function main() {
+  const lor = new HashMap();
+  lor.MAX_LOAD_RATIO = 0.5;
+  lor.SIZE_RATIO = 3;
+
+  lor.set('Hobbit','Bilbo');
+  lor.set('Hobbit','Frodo')
+  lor.set('Human','Aragon')
+  lor.set('Wizard','Gandolf')
+  lor.set('Elf','Legolas')
+  lor.set('Maiar','Sauron')
+  lor.set('LadyOfLight','Galadriel')
+  lor.set("RingBearer","Gollum")
+  lor.set("HalfElven","Arwen")
+  lor.set('Ent','Treebeard')
+
+  console.log(lor)
+  console.log(lor.get('Hobbit'))
+
+  // WhatDoesThisDo() 20 anb 10
+}
+
+main();
+
+// Maiar  = Sauron, and Hobbit is Frodo
+// This is because Bilbo is overwritten because they have the same key. This is not a collision because 
+// the keys are not unique
+
+// Capacity is 24.. This is because the MAX Load ratio was met and the hash table got resized. 
+
